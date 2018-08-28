c2c91daf78ff47618810765c3afd0b2e9c5c37e0
git-palace <edeveloper4@gmail.com>
2018-08-28 15:12:10 -0500

    updates

--- core/init.php
+++ core/init.php
@@ -5,17 +5,37 @@ add_shortcode( 'ninja_ajax_tables', function( $atts ) {
 		$atts
 	);
 
-	echo do_shortcode( '[ninja_tables id="' . $atts['id'] . '"]' );
+	$table_content = do_shortcode( '[ninja_tables id="' . $atts['id'] . '"]' );
+
+	$table_content = str_replace( 'ninja_table_pro', 'ninja_ajax_table_pro ninja_table_pro', $table_content );
+
+	echo $table_content;
 } );
 
 add_action( 'wp_enqueue_scripts', function() {
-	wp_enqueue_script( 'ninja_ajax_table', plugins_url( '../js/scripts.js', __FILE__ ), array( 'jquery' ), '1.0.0', true );
-	wp_localize_script( 'ninja-ajax-table-script', 'ninja_ajax_table', array( 'ajax_url' => admin_url( 'admin-ajax.php' ) ) );
+	wp_enqueue_script( 'ninja-ajax-table-script', plugins_url( '../js/scripts.js', __FILE__ ), array( 'jquery' ), '1.0.0', true );
+	wp_localize_script( 'ninja-ajax-table-script', 'ninja_ajax_tables', array( 'ajax_url' => admin_url( 'admin-ajax.php' ) ) );
 } );
 
 
 add_action( 'wp_ajax_update_ninja_ajax_tables', 'update_ninja_ajax_tables' );
 add_action( 'wp_ajax_nopriv_update_ninja_ajax_tables', 'update_ninja_ajax_tables' );
-function update_ninja_ajax_tables {
+function update_ninja_ajax_tables() {
+
+	$table_id = $_POST['table_id'];
+
+	$xml = simplexml_load_string( do_shortcode( '[ninja_tables id="' . $table_id . '"]' ) );
+
+	$table_content = '';
+
+	foreach ( $xml->table->tbody->tr as $tr_tag ) {
+
+		$table_content .= $tr_tag->asXML();
+
+	}
+
+	echo $table_content;
+
 	die;
+
 }
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,37 @@
-(function() {
+(function($) {
+
+	if ( $('table.ninja_ajax_table_pro').length ) {
+
+		setTimeout( function() {
+
+			$('table.ninja_ajax_table_pro').each( function( idx, selector ) {
+				
+				let table_id = $(selector).attr('id').split('_').pop();
+
+				let thead = $(selector).find('thead').html();
+
+				setInterval( function() {
+
+					let data = { 'action': 'update_ninja_ajax_tables', 'table_id': table_id };
+
+					$.post( ninja_ajax_tables.ajax_url, data, function( response ) {
+						
+						$(selector).find( 'tbody' ).html( response );
+
+						$(selector).footable();
+
+						if ( $(selector).find( 'thead' ).length )
+							$(selector).find( 'thead' ).html( thead );
+						else
+							$(selector).prepend( '<thead>' + thead + '</thead>' );
+
+					} );
+
+				}, 5000 );
+
+			}, 5000 );
+
+		} );
+	}
 
 })(jQuery);
